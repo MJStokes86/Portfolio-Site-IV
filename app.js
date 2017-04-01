e1b1dce6fea58023ca0551c90c708a95ced5fee6
Michael J. Stokes <mjstokes1986@att.net>
2017-04-01 03:05:05 -0400

    initial commit 3:04am

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+
+app.use(express.static(process.cwd() + '/public'));
+app.get('/', function(req,res){
+    res.send(fs.readFileSync('./views/index.html', 'utf8'));
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("LISTENING!")
+
+});
+
