febe485636815d7b883cab575edd51a80b2be37d
Michael J. Stokes <mjstokes1986@att.net>
2017-04-05 02:33:56 -0400

    checkpoint 2:33am

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,10 +1,4 @@
-/*
-* ----------------------------------------------------------------------------------------
-Author       : Creativepersonal
-Template Name: Designer - Personal Portfolio Template
-Version      : 1.0
-* ----------------------------------------------------------------------------------------
-*/
+
 (function ($) {
     'use strict';
 
@@ -156,23 +150,19 @@ Version      : 1.0
                     enabled: true
                 },
                 zoom: {
-                    enabled: true, // By default it's false, so don't forget to enable it
+                    enabled: true, 
+                    duration: 300, 
+                    easing: 'ease-in-out', 
 
-                    duration: 300, // duration of the effect, in milliseconds
-                    easing: 'ease-in-out', // CSS transition easing function
-
-                    // The "opener" function should return the element from which popup will be zoomed in
-                    // and to which popup will be scaled down
-                    // By defailt it looks for an image tag:
+               
                     opener: function (openerElement) {
-                        // openerElement is the element on which popup was initialized, in this case its <a> tag
-                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                       
                         return openerElement.is('img') ? openerElement : openerElement.find('img');
                     }
                 }
             });
         };
-        // Call the functions 
+        
         magnifPopup();
 
 
